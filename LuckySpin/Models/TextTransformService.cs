8c6db265b908ec31a5d57a4c1575f34b9ec4f828
Gregory Jesionowski <gregory.a.jesionowski@gmail.com>
2020-01-16 22:29:24 -0800

    Final Commit and Push for LuckySpin3!! Feeling like a wizard. Had to get help for making the Spin class work instead of ViewBag, but other than that it was pretty smooth sailing.

---
+++ LuckySpin/Models/TextTransformService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LuckySpin.Models
+{
+    public class TextTransformService
+    {
+
+        public string numberToText(int a)
+        {
+
+            int caseCheck = a;
+            string numText;
+            switch (caseCheck)
+            {
+                case 1:
+                    numText = "One";
+                    break;
+                case 2:
+                    numText = "Two";
+                    break;
+                case 3:
+                    numText = "Three";
+                    break;
+                case 4:
+                    numText = "Four";
+                    break;
+                case 5:
+                    numText = "Five";
+                    break;
+                case 6:
+                    numText = "Six";
+                    break;
+                case 7:
+                    numText = "Seven";
+                    break;
+                case 8:
+                    numText = "Eight"; 
+                    break;
+                case 9:
+                    numText = "Nine";
+                    break;
+                default:
+                    numText = "Number";
+                    break;
+            }
+
+            return numText;
+        }
+
+}
+}
